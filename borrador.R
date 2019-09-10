ca11256b6e0c6feb73b77f376495caaa93bd7da4
mentoldo <matias.alfonso@gmail.com>
2019-09-10 11:38:45 -0300

    Se avanzó con graficos de frecuencias

--- borrador.R
+++ borrador.R
@@ -14,3 +14,28 @@ url <- "https://www.indec.gob.ar/ftp/cuadros/menusuperior/enprecosp/bases_enprec
 download.file(url, "data/enprecops2011.rar")
 
 system("7z x ./data/enprecops2011.rar -o./data/enprecops/")
+
+
+## CEPRAM
+
+cepram <- read.csv2("./data/cuestionario.csv")
+
+cepram <- cepram[!is.na(cepram$TOMA),]
+
+names(cepram)
+cepram <- cepram[!names(cepram) == "DNI"]
+cepram <- cepram[!names(cepram) == "NRO"]
+
+
+cepram <- cepram[,!grepl("^CURSO", names(cepram))]
+cepram <- cepram[,!grepl("^PROFE", names(cepram))]
+
+names(cepram)[grepl("^CURSO", names(cepram))]
+names(cepram)[grepl("^PROFE", names(cepram))]
+
+write.csv2(cepram, row.names = FALSE, file = "./data/cepram.csv")
+
+
+##
+cepram <- read.csv2("./data/cepram.csv")
+names(cepram)
