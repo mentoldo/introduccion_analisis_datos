8c4d50ebb8e8521371d44eaebf1a03c172d26b63
mentoldo <matias.alfonso@gmail.com>
2019-10-15 18:20:20 -0300

    Se avanzó con Relaciones entre variables

--- seminario2.R
+++ seminario2.R
@@ -18,3 +18,31 @@ lista2<-lista[[1]]
 class(lista1)
 class(lista2)
 lista3<-lista[[3]]
+
+
+## BISG04. Durante los últimos 12 meses, a visitado a algún
+## profesional de la salud mental por problemas de angustia,
+## relaciones personales, depresión u otros?
+at_prof <- factor(enprecosp$BISG04,
+                  labels = c("Sí", "No", "Ns/Nc")
+)
+
+consumo <- factor(enprecosp$P1A_TA,
+                  labels = c("Sí", "No")
+)
+
+tabla <- table(at_prof, consumo)
+
+format(prop.table(tabla), digits = 1, scientific = FALSE)
+format(prop.table(tabla, 2), digits = 2, scientific = FALSE)
+format(prop.table(tabla, 1), digits = 3, scientific = FALSE)
+
+library(questionr)
+
+tabla <- wtd.table(at_prof, consumo, enprecosp$W_PERS)
+
+
+library(questionr)
+
+tc <- wtd.table(riesgo_perc, abuso, enprecosp$W_PERS)
+
